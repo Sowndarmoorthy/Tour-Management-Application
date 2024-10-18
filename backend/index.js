2655f007ac181c7732af94a2ec57f24e799d519a
Sowndar <sowundarmoorthy@gmail.com>
2024-10-18 18:15:33 +0530

    Updated backend cors settings

--- backend/index.js
+++ backend/index.js
@@ -4,12 +4,11 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const tourRoutes = require('./routes/tours');
-const userRoute = require("./routes/users.js")
+const userRoute = require("./routes/users.js");
 const authRoute = require("./routes/auth.js");
 const reviewRoute = require("./routes/reviews.js");
-const bookingRoute = require("./routes/bookings.js")
-const path = require('path')
-
+const bookingRoute = require("./routes/bookings.js");
+const path = require('path');
 
 dotenv.config({ path: path.resolve(__dirname, './.env') });
 
@@ -17,37 +16,37 @@ const app = express();
 
 app.use(cookieParser());
 
-
 const port = process.env.PORT || 8000;
-const corsOptions={
-    origin:'*',
-    credentials:true
-}
+
+const corsOptions = {
+    origin: 'https://tourister-o1md.onrender.com', // Your frontend URL
+    credentials: true // Allow credentials (cookies, authorization headers, etc.)
+};
+
 console.log('MongoDB URL:', process.env.JWT_SECRET_KEY);
 
-mongoose.set("strictQuery",false)
-const connect = async()=>{
-    try{
-        await mongoose.connect(process.env.MONGO_URL,{
-            useNewUrlParser:true,
-            useUnifiedTopology:true
-        }) 
-        console.log('MongoDb database connected');
-    }catch(err){
-        console.log(err)
+mongoose.set("strictQuery", false);
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log('MongoDB database connected');
+    } catch (err) {
+        console.log(err);
         console.log("MongoDB database connection failed");
     }
 }
 
-app.use(express.json());  
-app.use(cors(corsOptions));       
-app.use('/api/v1/tours',tourRoutes);
-app.use('/api/v1/users',userRoute);
+app.use(express.json());
+app.use(cors(corsOptions)); // Use CORS with the defined options
+
+app.use('/api/v1/tours', tourRoutes);
+app.use('/api/v1/users', userRoute);
 app.use('/api/v1/auth', authRoute);
-app.use('/api/v1/review',reviewRoute);
+app.use('/api/v1/review', reviewRoute);
 app.use('/api/v1/booking', bookingRoute);
-
-
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../frontend/build')));
@@ -56,7 +55,6 @@ if (process.env.NODE_ENV === 'production') {
         res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'));
     });
 }
-
 
 app.listen(port, () => {
     connect();
